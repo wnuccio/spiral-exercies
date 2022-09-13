8e1bc030e0b5781c8f617efbbe41d06947a0be9b
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-13 17:56:33 +0200

    generalize algorithm

--- src/main/java/spiral/Spiral.java
+++ src/main/java/spiral/Spiral.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static spiral.Spiral.Direction.*;
+
 
 public class Spiral {
     private final int[] row1;
@@ -22,14 +24,50 @@ public class Spiral {
         return true;
     }
 
+    enum Direction {
+        LEFT_RIGHT, UP_DOWN, RIGHT_LEFT, DOWN_UP
+    }
+
     public String asString() {
         List<Integer> l = new ArrayList<>();
-        for (int i=0; i < row1.length; i++) {
-            l.add(row1[i]);
+
+        int iMin=0, jMin=0;
+        int iMax = rows.length-1, jMax = rows.length-1;
+        int i=iMin, j=jMin;
+        Direction d = LEFT_RIGHT;
+        boolean notEnd = i != iMax;
+
+        boolean end = false;
+        while(notEnd) {
+            System.out.println(String.format("[%s, %s]", i, j));
+            l.add(rows[i][j]);
+
+            switch(d) {
+                case LEFT_RIGHT:
+                    if (j<jMax) { j++; break; }
+                    iMin++;
+                    d = UP_DOWN;
+
+                case UP_DOWN:
+                    if (i<iMax) { i++; break; }
+                    jMax--;
+                    d = RIGHT_LEFT;
+
+                case RIGHT_LEFT:
+                    if (j>jMin) { j--; break; }
+                    iMax--;
+                    d = DOWN_UP;
+
+                case DOWN_UP:
+                    if (i>iMin) { i--; break; }
+                    jMin++;
+                    d = LEFT_RIGHT;
+                    if (j == jMax) { end = true; } else j++;
+            }
+
+            notEnd = !end;
         }
-        for (int i=row2.length -1; i >= 0; i--) {
-            l.add(row2[i]);
-        }
+
 
         String result = l.stream()
                 .map(String::valueOf)
