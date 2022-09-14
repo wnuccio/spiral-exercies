fdef8e60f67b69d7e8fe3a16dcaa19261378e114
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-14 08:20:23 +0200

    remove unused fields

--- src/main/java/spiral/Spiral.java
+++ src/main/java/spiral/Spiral.java
@@ -8,20 +8,16 @@ import static spiral.Spiral.Direction.*;
 
 
 public class Spiral {
-    private final int[] row1;
-    private final int[] row2;
     private final int[][] rows;
 
+    private int i, iMin, iMax, j, jMin, jMax;
+    private Direction d;
+    private boolean end;
+    private List<Integer> numbers;
+
+
     public Spiral(int[][] rows) {
-        assert (isSquare(rows));
-
         this.rows = rows;
-        this.row1 = rows[0];
-        this.row2 = rows[1];
-    }
-
-    private boolean isSquare(int[][] rows) {
-        return true;
     }
 
     enum Direction {
@@ -29,48 +25,72 @@ public class Spiral {
     }
 
     public String asString() {
-        List<Integer> l = new ArrayList<>();
 
-        int iMin = 0, jMin = 0;
-        int iMax = rows.length - 1, jMax = rows.length - 1;
-        int i = iMin, j = jMin;
-        Direction d = RIGHT;
+        initialize();
 
         boolean end = false;
         while (!end) {
-            System.out.println(String.format("[%s, %s]", i, j));
-            l.add(rows[i][j]);
-
-            switch (d) {
-                case RIGHT:
-                    if (j < jMax) {j++;break;}
-                    iMin++;
-                    d = DOWN;
-
-                case DOWN:
-                    if (i < iMax) {i++;break;}
-                    jMax--;
-                    d = LEFT;
-
-                case LEFT:
-                    if (j > jMin) {j--;break;}
-                    iMax--;
-                    d = UP;
-
-                case UP:
-                    if (i > iMin) {i--;break;}
-                    jMin++;
-                    d = RIGHT;
-                    if (j < jMax) j++; else iMin++;
-            }
+            numbers.add(rows[i][j]);
+            moveOn();
             end = iMin == iMax && jMin == jMax;
         }
 
-        l.add(rows[i][j]);
+        numbers.add(rows[i][j]);
+        return joinAsString(numbers);
+    }
 
-        String result = l.stream()
+    private void moveOn() {
+        switch (d) {
+            case RIGHT:
+                if (j < jMax) {
+                    j++;
+                    break;
+                }
+                iMin++;
+                d = DOWN;
+
+            case DOWN:
+                if (i < iMax) {
+                    i++;
+                    break;
+                }
+                jMax--;
+                d = LEFT;
+
+            case LEFT:
+                if (j > jMin) {
+                    j--;
+                    break;
+                }
+                iMax--;
+                d = UP;
+
+            case UP:
+                if (i > iMin) {
+                    i--;
+                    break;
+                }
+                jMin++;
+                d = RIGHT;
+                if (j < jMax) j++;
+                else iMin++;
+        }
+    }
+
+    private void initialize() {
+        iMin = 0;
+        jMin = 0;
+        iMax = rows.length - 1;
+        jMax = rows.length - 1;
+        i = iMin;
+        j = jMin;
+        d = RIGHT;
+        numbers = new ArrayList<>();
+    }
+
+    private String joinAsString(List<Integer> l) {
+        return l.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining(" "));
-        return result;
     }
 }
