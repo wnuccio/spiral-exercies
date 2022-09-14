44fc17ba373aab694f32ac97fc6f6d958ef12758
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-14 10:30:17 +0200

    extract private methods for movement

--- src/main/java/spiral2/Matrix2.java
+++ src/main/java/spiral2/Matrix2.java
@@ -3,54 +3,61 @@ package spiral2;
 public class Matrix2 {
     private final int[][] values;
 
+    private int i, j;
+    private StringBuilder s;
+
     public Matrix2(int[][] values) {
         this.values = values;
+    }
+
+    private void moveRight() {
+        j++;
+        s.append(" " + values[i][j]);
+    }
+
+    private void moveDown() {
+        i++;
+        s.append(" " + values[i][j]);
+    }
+
+    private void moveLeft() {
+        j--;
+        s.append(" " + values[i][j]);
+    }
+
+    private void moveUp() {
+        i--;
+        s.append(" " + values[i][j]);
     }
 
     public String toSpiral() {
         if (values.length == 0) return "";
         if (values.length == 1) return "1";
 
-        StringBuilder s = new StringBuilder();
-        int i = 0, j = 0;
+        s = new StringBuilder();
+        i = 0;
+        j = 0;
 
         if (values.length == 2) {
             s.append(values[i][j]);
 
-            j++;
-            s.append(" " + values[i][j]);
-
-            i++;
-            s.append(" " + values[i][j]);
-
-            j--;
-            s.append(" " + values[i][j]);
+            moveRight();
+            moveDown();
+            moveLeft();
 
             return s.toString();
         }
 
         s.append(values[i][j]);
 
-        j++;
-        s.append(" " + values[i][j]);
-        j++;
-        s.append(" " + values[i][j]);
-
-        i++;
-        s.append(" " + values[i][j]);
-        i++;
-        s.append(" " + values[i][j]);
-
-        j--;
-        s.append(" " + values[i][j]);
-        j--;
-        s.append(" " + values[i][j]);
-
-        i--;
-        s.append(" " + values[i][j]);
-
-        j++;
-        s.append(" " + values[i][j]);
+        moveRight();
+        moveRight();
+        moveDown();
+        moveDown();
+        moveLeft();
+        moveLeft();
+        moveUp();
+        moveRight();
 
         return s.toString();
     }
