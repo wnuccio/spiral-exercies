d8c5787b4bcdcad53bf5f4466f32abe54334d02c
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-14 11:26:50 +0200

    generalize fourth movement in each scenario

--- src/main/java/spiral2/Matrix2.java
+++ src/main/java/spiral2/Matrix2.java
@@ -42,6 +42,9 @@ public class Matrix2 {
         move(direction, steps);
         direction = LEFT;
         move(direction, steps);
+        steps--;
+        direction = UP;
+        move(direction, steps);
 
         if (values.length == 0) {
 
@@ -50,22 +53,18 @@ public class Matrix2 {
         } else if (values.length == 2) {
 
         } else if (values.length == 3) {
-            move(UP, 1);
             move(RIGHT, 1);
         } else if (values.length == 4) {
-            move(UP, 2);
             move(RIGHT, 2);
             move(DOWN, 1);
             move(LEFT, 1);
         } else if (values.length == 5) {
-            move(UP, 3);
             move(RIGHT, 3);
             move(DOWN, 2);
             move(LEFT, 2);
             move(UP, 1);
             move(RIGHT, 1);
         } else if (values.length == 6) {
-            move(UP, 4);
             move(RIGHT, 4);
             move(DOWN, 3);
             move(LEFT, 3);
