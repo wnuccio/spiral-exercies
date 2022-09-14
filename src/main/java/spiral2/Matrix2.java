4d0cf57844ad8b41c875ace297aa29531e311e7f
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-14 11:14:59 +0200

    generalize move method for up

--- src/main/java/spiral2/Matrix2.java
+++ src/main/java/spiral2/Matrix2.java
@@ -12,25 +12,22 @@ public class Matrix2 {
         this.values = values;
     }
 
-    enum Direction {
-        RIGHT, DOWN, LEFT, UP
-    }
 
+    enum Direction {
+        RIGHT, DOWN, LEFT, UP;
+
+    }
     private void move(Direction direction, int steps) {
         for (int k=0; k<steps; k++) {
             switch (direction) {
                 case RIGHT: j++; break;
                 case DOWN: i++; break;
                 case LEFT: j--; break;
+                case UP: i--; break;
             }
             if (i == 0 && j == 0) s.append(values[i][j]);
             else s.append(" " + values[i][j]);
         }
-    }
-
-    private void moveUp() {
-        i--;
-        s.append(" " + values[i][j]);
     }
 
     public String toSpiral() {
@@ -50,13 +47,13 @@ public class Matrix2 {
             move(RIGHT, 3);
             move(DOWN, 2);
             move(LEFT, 2);
-            moveUp();
+            move(UP, 1);
             move(RIGHT, 1);
         } else if (values.length == 4) {
             move(RIGHT, 4);
             move(DOWN, 3);
             move(LEFT, 3);
-            moveUp();moveUp();
+            move(UP, 2);
             move(RIGHT, 2);
             move(DOWN, 1);
             move(LEFT, 1);
@@ -64,21 +61,21 @@ public class Matrix2 {
             move(RIGHT, 5);
             move(DOWN, 4);
             move(LEFT, 4);
-            moveUp();moveUp();moveUp();
+            move(UP, 3);
             move(RIGHT, 3);
             move(DOWN, 2);
             move(LEFT, 2);
-            moveUp();
+            move(UP, 1);
             move(RIGHT, 1);
         } else if (values.length == 6) {
             move(RIGHT, 6);
             move(DOWN, 5);
             move(LEFT, 5);
-            moveUp();moveUp();moveUp();moveUp();
+            move(UP, 4);
             move(RIGHT, 4);
             move(DOWN, 3);
             move(LEFT, 3);
-            moveUp();moveUp();
+            move(UP, 2);
             move(RIGHT, 2);
             move(DOWN, 1);
             move(LEFT, 1);
