11282a994cf097ee4387c1f716cbec330af8c67c
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-14 11:52:23 +0200

    reformatting

--- src/main/java/spiral2/Matrix2.java
+++ src/main/java/spiral2/Matrix2.java
@@ -5,32 +5,19 @@ import static spiral2.Matrix2.Direction.*;
 public class Matrix2 {
     private final int[][] values;
 
+    enum Direction {
+        RIGHT, DOWN, LEFT, UP
+    }
+
     private int i, j;
-    int steps;
-    Direction direction;
+    private int steps;
+    private Direction direction;
+
     private StringBuilder s;
 
     public Matrix2(int[][] values) {
         this.values = values;
     }
-
-    enum Direction {
-        RIGHT, DOWN, LEFT, UP
-
-    }
-    private void move(Direction direction, int steps) {
-        for (int k=0; k<steps; k++) {
-            switch (direction) {
-                case RIGHT: j++; break;
-                case DOWN: i++; break;
-                case LEFT: j--; break;
-                case UP: i--; break;
-            }
-            if (i == 0 && j == 0) s.append(values[i][j]);
-            else s.append(" ").append(values[i][j]);
-        }
-    }
-
     public String toSpiral() {
         initialize();
 
@@ -42,6 +29,19 @@ public class Matrix2 {
         }
 
         return s.toString();
+    }
+
+    private void move(Direction direction, int steps) {
+        for (int k=0; k<steps; k++) {
+            switch (direction) {
+                case RIGHT: j++; break;
+                case DOWN: i++; break;
+                case LEFT: j--; break;
+                case UP: i--; break;
+            }
+            if (i == 0 && j == 0) s.append(values[i][j]);
+            else s.append(" ").append(values[i][j]);
+        }
     }
 
     private void moveUp() {
