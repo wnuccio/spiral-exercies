b6c12b89074d3916fd7945d29ba612814889fbee
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-14 08:54:19 +0200

    try another incremental approach with Matrix2 class

---
+++ src/test/java/spiral2/Matrix2.java
@@ -0,0 +1,11 @@
+package spiral2;
+
+public class Matrix2 {
+    public Matrix2(int[][] values) {
+
+    }
+
+    public String toSpiral() {
+        return "1";
+    }
+}
---
+++ src/test/java/spiral2/Matrix2Test.java
@@ -0,0 +1,17 @@
+package spiral2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class Matrix2Test {
+    @Test
+    void return_1_x_1_spiral() {
+        Matrix2 matrix = new Matrix2(new int[][] {
+                new int[]{1}
+        });
+
+        assertEquals("1", matrix.toSpiral());
+    }
+}
