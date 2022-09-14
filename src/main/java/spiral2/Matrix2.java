679e42c5c12d0711fa991e44baf8689819a42acd
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-14 11:29:57 +0200

    generalize sixth movement in each scenario

--- src/main/java/spiral2/Matrix2.java
+++ src/main/java/spiral2/Matrix2.java
@@ -49,6 +49,9 @@ public class Matrix2 {
         move(direction, steps);
         direction = RIGHT;
         move(direction, steps);
+        steps--;
+        direction = DOWN;
+        move(direction, steps);
 
         if (values.length == 0) {
 
@@ -59,15 +62,12 @@ public class Matrix2 {
         } else if (values.length == 3) {
 
         } else if (values.length == 4) {
-            move(DOWN, 1);
             move(LEFT, 1);
         } else if (values.length == 5) {
-            move(DOWN, 2);
             move(LEFT, 2);
             move(UP, 1);
             move(RIGHT, 1);
         } else if (values.length == 6) {
-            move(DOWN, 3);
             move(LEFT, 3);
             move(UP, 2);
             move(RIGHT, 2);
