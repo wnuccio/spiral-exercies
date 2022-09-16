8a2d9ddf483ff27cd2e9f8f6f9ec5d24ae9dc81f
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-16 19:48:18 +0200

    [zigzag] add some test cases

--- src/test/java/zigzag/ZigZagTest.java
+++ src/test/java/zigzag/ZigZagTest.java
@@ -6,14 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ZigZagTest {
     @Test
-    void convert_on_one_row() {
-        assertEquals("A", new ZigZag("A", 1).convert());
-        assertEquals("AB", new ZigZag("AB", 1).convert());
-        assertEquals("ABC", new ZigZag("ABC", 1).convert());
-        assertEquals("ABCD", new ZigZag("ABCD", 1).convert());
-    }
-
-    @Test
     void convert_when_letters_are_not_more_than_rows() {
         assertEquals("A", new ZigZag("A", 1).convert());
         assertEquals("A", new ZigZag("A", 2).convert());
@@ -24,10 +16,29 @@ public class ZigZagTest {
     }
 
     @Test
-    void convert_many_letters_on_two_rows() {
+    void convert_on_one_row() {
+        assertEquals("A", new ZigZag("A", 1).convert());
+        assertEquals("AB", new ZigZag("AB", 1).convert());
+        assertEquals("ABC", new ZigZag("ABC", 1).convert());
+        assertEquals("ABCD", new ZigZag("ABCD", 1).convert());
+    }
+
+    @Test
+    void convert_on_two_rows() {
+        assertEquals("A", new ZigZag("A", 2).convert());
+        assertEquals("AB", new ZigZag("AB", 2).convert());
         assertEquals("ACB", new ZigZag("ABC", 2).convert());
         assertEquals("ACBD", new ZigZag("ABCD", 2).convert());
         assertEquals("ACEBD", new ZigZag("ABCDE", 2).convert());
+    }
+
+    @Test
+    void convert_on_three_rows() {
+//        assertEquals("A", new ZigZag("ABC", 3).convert());
+//        assertEquals("ACB", new ZigZag("ABC", 3).convert());
+//        assertEquals("ACB", new ZigZag("ABC", 3).convert());
+//        assertEquals("ACBD", new ZigZag("ABCD", 3).convert());
+//        assertEquals("ACEBD", new ZigZag("ABCDE", 3).convert());
     }
 
     /*
