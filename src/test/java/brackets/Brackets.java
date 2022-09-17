e9fefcb4daf5ff24364db2317f9662bad5e64c4b
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-17 19:26:30 +0200

    [brackets] check all 4-length strings

--- src/test/java/brackets/Brackets.java
+++ src/test/java/brackets/Brackets.java
@@ -9,27 +9,14 @@ public class Brackets {
 
     public boolean isValid() {
         if (s.isEmpty()) return true;
-        if (s.length() == 1) {
-            return false;
+        if (s.charAt(0) != '(') return false;
+        int numOfOpenBrackets = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') numOfOpenBrackets++;
+            else if (ch == ')') numOfOpenBrackets--;
+            else throw new IllegalArgumentException("Invalid char: " + ch);
         }
-        if (s.length() == 2) {
-            if (s.charAt(0) != '(') return false;
-            return s.charAt(1) == ')';
-        }
-        if (s.length() == 3) {
-            return false;
-        }
-        if (s.length() == 4) {
-            if (s.charAt(0) != '(') return false;
-            int numOfOpenBrackets = 0;
-            for (char ch: s.toCharArray()) {
-                if (ch == '(') numOfOpenBrackets++;
-                else if (ch == ')') numOfOpenBrackets--;
-                else throw new IllegalArgumentException("Invalid char: " +ch);
-            }
-            return numOfOpenBrackets == 0;
-        }
-        return false;
+        return numOfOpenBrackets == 0;
     }
 
     public boolean isNotValid() {
