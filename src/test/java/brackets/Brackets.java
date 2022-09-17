79fee42e8f81d3eaa7942c2b3a3abfe9e0b3d880
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-17 19:04:21 +0200

    [brackets] simplify the algorithm

--- src/test/java/brackets/Brackets.java
+++ src/test/java/brackets/Brackets.java
@@ -15,15 +15,10 @@ public class Brackets {
         "(()" => no, only even length;
      */
     private boolean isValid(String str, int start, int end) {
-        if (start == 0 &&  end == -1) return true; // empty string
+        if (start > end) return true; // empty string
 
         boolean isOddLength = (end - start) % 2 != 1;
         if(isOddLength) return false;
-
-        // two char string
-        if (start == end-1) {
-            return str.charAt(start) == '(' && str.charAt(end) == ')'; // empty string
-        }
 
         if (str.charAt(start) != '(' || str.charAt(end) != ')') return false;
 
