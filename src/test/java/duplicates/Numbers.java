dcfab58611ce9f7fcf0b53ef3b08514903a8294e
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-18 09:33:39 +0200

    [duplicates] refactor

--- src/test/java/duplicates/Numbers.java
+++ src/test/java/duplicates/Numbers.java
@@ -2,23 +2,31 @@ package duplicates;
 
 public class Numbers {
     public static int removeDuplicates(int[] nums) {
-        int k;
-        if (nums.length == 2) {
-            if (nums[0] == nums[1]) return 1;
-            else return 2;
-         }
-        if (nums.length == 3) { // [100, 101, 101]
+        int k = -1;
+        if (nums.length == 1) {
+            if (nums[0] == nums[0]) {
+                k = 1;
+            } else {
+                k = -1;
+            }
+        } else if (nums.length == 2) {
+            if (nums[0] == nums[1]) {
+                k = 1;
+            } else if (nums[0] != nums[1]) {
+                k = 2;
+            }
+         } else if (nums.length == 3) { // [100, 101, 101]
             if (nums[0] == nums[1] && nums[1] == nums[2]) {
-                return 1;
+                k = 1;
             } else if (nums[0] == nums[1] && nums[1] != nums[2]) {
                 nums[1] = nums[2];
-                return 2;
+                k = 2;
             } else if (nums[0] != nums[1] && nums[1] == nums[2]) {
-                return 2;
+                k = 2;
             } else if (nums[0] != nums[1] && nums[1] != nums[2]) {
-                return 3;
+                k = 3;
             }
          }
-        return 1;
+        return k;
     }
 }
