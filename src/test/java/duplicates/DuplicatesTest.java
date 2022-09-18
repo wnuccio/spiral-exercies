aad774fc03d923cd99c790c1463a06e9768ccc20
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-18 09:16:14 +0200

    [duplicates] two numbers

--- src/test/java/duplicates/DuplicatesTest.java
+++ src/test/java/duplicates/DuplicatesTest.java
@@ -6,11 +6,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DuplicatesTest {
 
+    /*
+        [1, 1, 2, 2, 2, 3, 5, 5] => [1, 2, 3, 5] , k: 4
+
+        lastUniqueNum: value of the last unique number in nums
+        m: index of current modifiable position in nums
+        currNum:
+
+        [1, 2, ...]   k: 2
+        [1, 2, 3 ...] k: 3
+
+        currNum == lastUniqueNum ? => nothing to do
+        currNum > lastUniqueNum ? =>
+            nums[m] = currNum
+            m++;
+            lastUniqueNum = currNum
+
+     */
     @Test
     void one_numbers_remains_the_same() {
         int[] nums = new int[]{100};
         int k = Numbers.removeDuplicates(nums);
-        assertEquals(1, k);
-        assertEquals(100, nums[0]);
+
+        assertEqualNumbers(new int[]{100}, nums, k);
+    }
+
+    @Test
+    void two_equals_numbers() {
+        int[] nums = new int[]{100, 100};
+        int k = Numbers.removeDuplicates(nums);
+
+        assertEqualNumbers(new int[]{100}, nums, k);
+    }
+
+    @Test
+    void two_different_numbers() {
+        int[] nums = new int[]{100, 101};
+        int k = Numbers.removeDuplicates(nums);
+
+        assertEqualNumbers(new int[]{100, 101}, nums, k);
+    }
+
+    private void assertEqualNumbers(int[] expected, int[] nums, int k) {
+        assertEquals(expected.length, k);
+        for (int i = 0; i < k; i++)
+            assertEquals(expected[i], nums[i]);
     }
 }
--- src/test/java/duplicates/Numbers.java
+++ src/test/java/duplicates/Numbers.java
@@ -2,6 +2,10 @@ package duplicates;
 
 public class Numbers {
     public static int removeDuplicates(int[] nums) {
+        if (nums.length == 2) {
+            if (nums[0] == nums[1]) return 1;
+            else return 2;
+         }
         return 1;
     }
 }
