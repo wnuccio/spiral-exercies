ee761e683399194517ab935bbb69a9c565caf29c
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-18 08:36:32 +0200

    [prefix] simplify iteration

--- src/test/java/prefix/Words.java
+++ src/test/java/prefix/Words.java
@@ -4,6 +4,7 @@ public class Words {
     private String[] s;
 
     public Words(String... s) {
+        if (s.length == 0) throw new IllegalArgumentException();
         this.s = s;
     }
 
@@ -15,11 +16,15 @@ public class Words {
     }
 
     private boolean sameCharAtIndex(int index, String[] words) {
-        if (words.length <= 1) return true;
+        if (words.length == 1) return true;
+
         for (int wordIndex = 0; wordIndex < words.length - 1; wordIndex++) {
             String word = words[wordIndex];
             String nextWord = words[wordIndex + 1];
-            if (word.charAt(index) != nextWord.charAt(index)) return false;
+            if (index >= word.length() || index >= nextWord.length())
+                return false;
+            if (word.charAt(index) != nextWord.charAt(index))
+                return false;
         }
         return true;
     }
@@ -27,12 +32,11 @@ public class Words {
     public String commonPrefix() {
         int i = 0;
         String result = "";
-        boolean endOfStrings = endOfAnyWordReached(i, s);
-        while (!endOfStrings) {
-            if ( !sameCharAtIndex(i, s)) return result;
+        while (true) {
+            if (!sameCharAtIndex(i, s)) break;
+            if (i >= s[0].length()) break;
             result += s[0].charAt(i);
             i++;
-            endOfStrings = endOfAnyWordReached(i, s);
         }
         return result;
     }
