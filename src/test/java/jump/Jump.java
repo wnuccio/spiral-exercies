50a6ef8c1b9b8ab210aab68a9e604e0abff6c26e
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-18 15:12:15 +0200

    [jump] optimize for large array

--- src/test/java/jump/Jump.java
+++ src/test/java/jump/Jump.java
@@ -8,11 +8,30 @@ package jump;
  */
 public class Jump {
 
+    private static int tryAllJumpLengths(int maxJumpLength, int currentJumps, int[] nums, int currentIndex) {
+        int minRemainingJumps = -1;
+        for (int i = maxJumpLength; i >= 1; i--) {
+            int nextJumps = minJumps(currentJumps, nums, currentIndex + i);
+            if (minRemainingJumps == 1) {
+                minRemainingJumps = 1;
+                break;
+            }
+
+            if (nextJumps == -1)
+                continue;
+            if (nextJumps < minRemainingJumps || minRemainingJumps == -1)
+                minRemainingJumps = nextJumps;
+        }
+        return minRemainingJumps;
+    }
+
     private static int minJumps(int currentJumps, int[] nums, int currentIndex) {
-        if (currentIndex > nums.length - 1)
+        int endIndex = nums.length - 1;
+
+        if (currentIndex > endIndex)
             return -1;
 
-        if (currentIndex == nums.length - 1)
+        if (currentIndex == endIndex)
             return currentJumps + 0;
 
         int maxJumpLength = nums[currentIndex];
@@ -20,14 +39,11 @@ public class Jump {
         if (maxJumpLength == 0)
             return -1;
 
-        int minRemainingJumps = -1;
-        for (int i = maxJumpLength; i >= 1; i--) {
-            int nextJumps = minJumps(currentJumps, nums, currentIndex + i);
-            if (nextJumps == -1)
-                continue;
-            if (nextJumps < minRemainingJumps || minRemainingJumps == -1)
-                minRemainingJumps = nextJumps;
-        }
+        if (endIndex - currentIndex <= maxJumpLength)
+            return currentJumps + 1;
+
+        int minRemainingJumps = tryAllJumpLengths(maxJumpLength, currentJumps, nums, currentIndex);
+
         return (minRemainingJumps == -1) ? -1 : currentJumps + 1 + minRemainingJumps ;
     }
 
--- src/test/java/jump/JumpTest.java
+++ src/test/java/jump/JumpTest.java
@@ -51,4 +51,9 @@ public class JumpTest {
         assertEquals(1, Jump.jump(new int[]{5, 0, 0, 0, 0, 1}));
         assertEquals(-1, Jump.jump(new int[]{4, 0, 0, 0, 0, 1}));
     }
+
+    @Test
+    void large_arrays() {
+        assertEquals(5, Jump.jump(new int[]{5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5}));
+    }
 }
