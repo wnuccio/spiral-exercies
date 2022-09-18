53cef4a19b4a11aa151684629c08da715fe70c17
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-18 13:13:00 +0200

    [jump] three length simple case

--- src/test/java/jump/Jump.java
+++ src/test/java/jump/Jump.java
@@ -1,5 +1,7 @@
 package jump;
-
+/*
+    [1, 1, 1]
+ */
 public class Jump {
     public static int jump(int[] nums) {
         int i=0;
@@ -8,6 +10,11 @@ public class Jump {
         if (nums.length == 2 && nums[0] > 0)
             return 1;
 
+        if (nums.length == 3) {
+            if (nums[0] == 0)
+                return -1;
+            return 2;
+        }
         return -1;
     }
 }
--- src/test/java/jump/JumpTest.java
+++ src/test/java/jump/JumpTest.java
@@ -12,14 +12,16 @@ public class JumpTest {
     }
 
     @Test
-    void one_jump_for_two_length_array() {
+    void jump_for_two_length_array() {
+        assertEquals(-1, Jump.jump(new int[]{0, 1}));
         assertEquals(1, Jump.jump(new int[]{1, 1}));
         assertEquals(1, Jump.jump(new int[]{1, 2}));
         assertEquals(1, Jump.jump(new int[]{2, 1}));
     }
 
     @Test
-    void error_for_two_length_array_starting_with_0() {
-        assertEquals(-1, Jump.jump(new int[]{0, 1}));
+    void three_length_array() {
+        assertEquals(-1, Jump.jump(new int[]{0, 1, 1}));
+        assertEquals(2, Jump.jump(new int[]{1, 1, 1}));
     }
 }
