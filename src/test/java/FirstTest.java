163298846f0f239a0d5e8d417217b346e50e4e0e
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-19 15:37:46 +0200

    [interview] first test

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstTest {
+    @Test
+    void test() {
+        assertEquals(1, 1);
+    }
+}
